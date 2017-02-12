906f4f4ab8106075b0f0a72835cf9fa39566109e
Bambe Chen <ayuha406645@gmail.com>
2017-02-12 09:56:40 +0800

    02 新增 Page2Component，並且新增 Tracing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
+import { Page2Component } from './page2/page2.component';
 import { LayoutComponent } from './layout/layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'layout',
+    path: '',
     component: LayoutComponent,
     children: []
+  },
+  {
+    path: 'page2',
+    component: Page2Component
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    enableTracing: true,
+  })],
   exports: [RouterModule],
   providers: []
 })
